b2091ddba4ae93f714f19a74242ee08a9398a456
IceEdgar <125385556+IceEdgar@users.noreply.github.com>
2023-02-25 16:06:55 +0500

    Add files via upload

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[] array = { 12, 33, 3344, 54, 3, 22, 77, 55, 99 };
+
+int max = Max(Max(array[0], array[1], array[2]),
+          Max(array[3], array[4], array[5]),
+          Max(array[6], array[7], array[8]));
+
+
+//int max=Max(Max(a1, b1, c1),Max(a2, b2, c2),Max(a3, b3, c3));
+
+Console.WriteLine(max);
---
+++ Example010_Method_Array/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 2, 3, 4, 66, 33, 6, 66 };
+
+int n = array.Length;
+int find = 66;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index=index+1;
+    index++;
+}
